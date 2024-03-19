5f66fae6a81df39ff8836bada4deeee59d761ae3
Shivani yadav <94887322+Shivaniyadav2910@users.noreply.github.com>
2024-03-19 08:18:22 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,7 +17,7 @@ let valueInNumber =Number (score)
 //"33abc"=>NaN
 //"true"=>1; false=>0
 
-// let isLoggedIn=1;  //true
+let isLoggedIn=1;  //true
 // let isLoggedIn="";  //false
 // let isLoggedIn="shivani";  //ture
 let booleanIsLoggedIn= Boolean(isLoggedIn);
@@ -30,5 +30,39 @@ console.log(booleanIsLoggedIn);
 let someNumber=33;
 
 let sNumber=String(someNumber)
-console.log(sNumber);
-console.log(typeof sNumber);
+// console.log(sNumber);
+// console.log(typeof sNumber);
+
+//****************************Operation********************* */
+let value=3
+let negValue=-value;
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello";
+let str2="shivani";
+
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+
+console.log((3+4)*5 % 3);
+
+// console.log(+ture);
+// console.log(+"");
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+// gameCounter++;
+++gameCounter;
+// console.log(gameCounter); //101
